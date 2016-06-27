75f7ed1af21a0909fe7e11317891a973b9575262
Adam Lyon <lyon@fnal.gov>
2016-06-27 11:23:00 -0500

    update for webdav

--- xcode-tools/build.sh
+++ xcode-tools/build.sh
@@ -11,5 +11,12 @@
 #   to be a gm2.env file there
 
 # Execute in the container (use bash -l to load .bashrc)
+
+# Get location of this script, because gm2_fcns.sh is there too
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker exec $1 bash -c -l "source $DIR/gm2_fcns.sh ; cd $PWD ; restore_gm2 ; ninja"
+
+# Execute the build in the container
+# Note the use of "pipefail" -- see http://stackoverflow.com/questions/1221833/bash-pipe-output-and-capture-exit-status
+#   That allows the exit status of ninja to trump the likely aways sucessful sed
+docker exec $1 bash -c -l \
+  "source $DIR/gm2_fcns.sh ; cd $MRB_BUILDDIR ; restore_gm2 ; ( set -o pipefail ; ninja 2>&1 | sed \"s#${MRB_SOURCE}#${PWD}#g\" ) "
